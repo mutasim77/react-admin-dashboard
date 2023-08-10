0e0677f4847a460ec6ad628ac2873353a9782c38
Mutasim <qmutasim77@gmail.com>
2023-08-10 20:15:02 +0600

    project done

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>mutasim</span>
+      <span>© Mutasim Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
